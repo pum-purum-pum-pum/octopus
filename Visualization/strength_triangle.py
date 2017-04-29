c079fd537fb8e961ee258819d5020e2ebbb906ca
Vlad Zhukov <VladZhukovTirko@gmail.com>
2017-04-29 18:59:49 +0300

    add_win_rate

--- Visualization/strength_triangle.py
+++ Visualization/strength_triangle.py
@@ -16,6 +16,24 @@ parser.add_argument('--features', type=str, default='')
 args = parser.parse_args()
 
 
+def solo_name(name):
+    return not '/' in name
+players_data = pd.read_csv('../data/players_atp.csv')
+select_names = np.array([solo_name(i) for i in players_data['NAME_P']])
+players_data = players_data[select_names]
+
+
+def get_win_rate(pid1, pid2):
+    games_data = pd.read_csv(os.path.dirname(os.path.realpath(__file__)) + '/../data/games_atp.csv')
+    players_data = pd.read_csv(os.path.dirname(os.path.realpath(__file__)) + '/../data/players_atp.csv')
+    wins = pd.merge(players_data, games_data, left_on='ID_P', right_on='ID1_G').groupby('ID_P').size()
+    loses = pd.merge(players_data, games_data, left_on='ID_P', right_on='ID2_G').groupby('ID_P').size()
+    win_los = pd.concat([wins, loses], axis=1)
+    win_los.columns = ['wins', 'loses']
+    win_los['win_rate'] = win_los['wins'] / (win_los['loses'] + win_los['wins'])
+    return win_los.iloc[win_los.index ==pid1]['win_rate'].values[0], \
+            win_los.iloc[win_los.index ==pid2]['win_rate'].values[0]
+
 def strength_triangle(id_1, id_2, tournament=None, other = None):
     p_data = pd.read_csv(os.path.dirname(os.path.realpath(__file__)) + '/../data/players_atp.csv')
     p_data = p_data.dropna(subset = ['RANK_P', 'POINT_P', 'PRIZE_P'])
@@ -35,8 +53,10 @@ def strength_triangle(id_1, id_2, tournament=None, other = None):
     point_p2_norm = point_p2/point_norm
     prize_p1_norm = prize_p1/prize_norm
     prize_p2_norm = prize_p2/prize_norm
-    visualization_data = [[rank_p1_norm, point_p1_norm, prize_p1_norm],[rank_p2_norm, point_p2_norm, prize_p2_norm]]
-    visualization_legend = {'axis':['Rank', 'Points', 'Prize'], 'title':'strength polygon'}
+    wr_p1, wr_p2 =  get_win_rate(id_1, id_2)
+    visualization_data = [[rank_p1_norm, point_p1_norm, prize_p1_norm, wr_p1],
+                          [rank_p2_norm, point_p2_norm, prize_p2_norm, wr_p2]]
+    visualization_legend = {'axis': ['Rank', 'Points', 'Prize', 'Win Rate'], 'title': 'Strength polygon'}
     r = dict(type = visualization_type, data = visualization_data, legend = visualization_legend)
     return r
 
