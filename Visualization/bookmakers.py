8a73b11df14be4edaea9846dd91779843d8ec903
Vlad Zhukov <VladZhukovTirko@gmail.com>
2017-04-29 15:15:45 +0300

    fix directory and epilog

--- Visualization/bookmakers.py
+++ Visualization/bookmakers.py
@@ -2,11 +2,12 @@ import numpy as np
 import pandas as pd
 import argparse
 import json
+import os
 
 
 epilog = \
     "EXAMPLE of usage:\n" +\
-    "python bookmakers.py --id1 1 --id2"
+    "python bookmakers.py --id1 9 --id2 27"
 
 parser = argparse.ArgumentParser(description='input output files', epilog=epilog)
 parser.add_argument('--id1', type=int, help="player1 id")
@@ -15,7 +16,7 @@ args = parser.parse_args()
 
 
 if __name__ == "__main__":
-    data_book_makers = pd.read_csv('../data/odds_atp.csv')
+    data_book_makers = pd.read_csv(os.path.dirname(os.path.realpath(__file__)) + '/../data/odds_atp.csv')
     koef = data_book_makers.loc[(data_book_makers['ID1_O'] == args.id1)\
                          & (data_book_makers['ID2_O'] == args.id2)][['K1', 'K2']].mean(axis=0)
     if np.isnan(koef[0]) or np.isnan(koef[1]):
