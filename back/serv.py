12a17e95b32820386922e2eb5eac693f1fc450cf
Vlad Zhukov <VladZhukovTirko@gmail.com>
2017-05-02 00:17:47 +0300

    fix

--- back/serv.py
+++ back/serv.py
@@ -6,6 +6,8 @@ import numpy as np
 import sqlite3
 app = Flask(__name__)
 con = sqlite3.connect('../data/on_court.db')
+cursor = con.cursor()
+cursor.execute("SELECT * FROM sqlite_master")
 
 
 @app.route('/')
@@ -14,8 +16,6 @@ def hello_world():
 
 @app.route('/bookmakers', methods=['GET'])
 def get_bookmakers():
-    cursor = con.cursor()
-    cursor.execute("SELECT * FROM sqlite_master")
     player1 = request.args.get('player1')
     player2 = request.args.get('player2')
     return get_bookmakers_coef(cursor, player1, player2)
