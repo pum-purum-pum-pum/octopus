7e05f863c6f2d4e3695b559c7f0d5d10a9fedc65
ale3otik <aleksey.zotov@phystech.edu>
2017-05-23 11:12:56 +0300

    change trinagle

--- Visualization/strength_triangle.py
+++ Visualization/strength_triangle.py
@@ -30,6 +30,13 @@ def get_win_rate(pid1, pid2):
     return win_los.iloc[win_los.index ==pid1]['win_rate'].values[0], \
             win_los.iloc[win_los.index ==pid2]['win_rate'].values[0]
 
+def normed_1(value, max_value) :
+    return 1.0 - np.log(value) / np.log(max_value)
+def normed_2(value, max_value) :
+    return (1 + np.log(value)) / (1 + np.log(max_value))
+def normed_octo(value,max_value, min_value) :
+    return (value - min_value)/(max_value - min_value)
+
 def strength_triangle(id_1, id_2, tournament=None, other = None):
     p_data = pd.read_csv(os.path.dirname(os.path.realpath(__file__)) + '/../data/players_atp.csv')
     p_data = p_data.dropna(subset = ['RANK_P', 'POINT_P', 'PRIZE_P'])
@@ -50,13 +57,37 @@ def strength_triangle(id_1, id_2, tournament=None, other = None):
     # prize_p1_norm = prize_p1/prize_norm
     # prize_p2_norm = prize_p2/prize_norm
     wr_p1, wr_p2 =  get_win_rate(id_1, id_2)
-    visualization_data = [[1 - np.log(rank_p1) / np.log(rank_norm), point_p1_norm,
-                           (1 + np.log(prize_p1)) / (1 + np.log(prize_norm)), wr_p1],
-                          [1 - np.log(rank_p2) / np.log(rank_norm), point_p2_norm,
-                           (1 + np.log(prize_p2)) / (1 + np.log(prize_norm)), wr_p2]]
-    visualization_legend = {'axis': ['Rank', 'Points', 'Prize', 'Win Rate'], 'title': 'Strength polygon'}
-    r = dict(type = visualization_type, data = visualization_data, legend = visualization_legend)
-    return r
+
+    octorate_data = pd.read_csv(os.path.dirname(os.path.realpath(__file__)) + '/../data/octorate.csv')
+    octorate_max = np.max(octorate_data['mu'].values)
+    octorate_min = np.min(octorate_data['mu'].values)
+
+    octorate_v1 = octorate_data[octorate_data['ID']==id_1]['mu'].values[0]
+    octorate_v2 = octorate_data[octorate_data['ID']==id_2]['mu'].values[0]
+
+    octorate_v1_normed = normed_octo(octorate_v1,octorate_max,octorate_min)
+    octorate_v2_normed = normed_octo(octorate_v2,octorate_max,octorate_min)
+
+    visualization_data_normed = [   [normed_1(rank_p1,rank_norm), point_p1_norm,
+                                            normed_2(prize_p1,prize_norm),
+                                            wr_p1,octorate_v1_normed],
+                                    [normed_1(rank_p2,rank_norm), point_p2_norm,
+                                            normed_2(prize_p2,prize_norm),
+                                            wr_p2,octorate_v2_normed] ]
+
+    visualization_data_original = [ [rank_p1, point_p1, prize_p1, wr_p1, octorate_v1],
+                                    [rank_p2, point_p2, prize_p2, wr_p2, octorate_v2] ]
+
+    visualization_legend = {'axis': ['Rank', 'Points', 'Prize', 'Win Rate', 'Octo Rate'], 'title': 'Strength polygon'}
+
+    r_normed = dict(    type = visualization_type,
+                        data = visualization_data_normed,
+                        legend = visualization_legend)
+
+    r_original = dict(  type = visualization_type,
+                        data = visualization_data_original,
+                        legend = visualization_legend)
+    return [r_normed,r_original]
 
 if __name__ == "__main__":
     try:
